1b3cd61f82f0f2e3a42c44fd74f605af9bed9e98
Rubi <rer1288@rit.edu>
2023-10-22 17:08:05 -0400

    question 21

---
+++ question21.py
@@ -0,0 +1,35 @@
+import random
+
+def random_counts(n):
+    d = dict()
+    
+    while len(d) < n+1:
+        x = random.randint(0, n)
+        if x in d:
+            d[x] += 1
+        else:
+            d[x] = 1
+
+    return d
+
+def main():
+    random.seed(7)
+    d = random_counts(10000)
+    max = 0
+    count = 0
+    for x in d:
+        count += d[x]
+        if d[x] > max:
+            max =  d[x]
+    print('count:', count)
+    print('max:', max)
+
+    l = []
+
+    for x in d:
+        if d[x] == max:
+            l.append(x)
+    l.sort()
+    print(l)
+
+main()
