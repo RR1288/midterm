741e3c03ecb48a3317d8ce23f9e6338b2774fa03
Rubi <rer1288@rit.edu>
2023-10-22 13:47:56 -0400

    question 3

---
+++ question3.py
@@ -0,0 +1,11 @@
+def make_tuple(a, b, c):
+    return tuple([a, b, c])
+
+def main():
+    tup = make_tuple(1,  2, 3)
+    print(tup)
+    tup = make_tuple(5, 8, 11)
+    print(tup)
+
+if __name__  == "__main__":
+    main()
