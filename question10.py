d35ca378979ae8a8f42b384d6c053d07552fab29
Rubi <rer1288@rit.edu>
2023-10-22 15:02:48 -0400

    question 12

--- question10.py
+++ question10.py
@@ -8,6 +8,18 @@ def splice(a, b):
     for i in range(len(b)):
         a.append(b[i])
 
+def scramble(a_string):
+    l = []
+    for i in range(len(a_string)):
+        l.append(a_string[i:i+1])
+    backwards = ''
+    for i in range(len(l)):
+        a = (l[len(l)-i - 1:len(l)-i])
+        backwards += a[0]
+    print(backwards)
+    return backwards
+
+
 def main():
     l = [3,4,1,2,5]
     b = ['a', 'b']
@@ -17,6 +29,11 @@ def main():
 
     splice(l, b)
     print(l)
+
+    l = 'This is a scramble test'
+    l = scramble(l)
+    l = scramble(l)
+
 
 
 main()
