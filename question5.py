b3289b7c968e41c732e267edd8fd7ed3e4f63dec
Rubi <rer1288@rit.edu>
2023-10-22 13:57:32 -0400

    question 5 and 6

---
+++ question5.py
@@ -0,0 +1,23 @@
+def make_trading_card(name, mana_value, power, toughness):
+    return tuple([name, mana_value, power, toughness])
+
+def trading_value(card):
+    return card[2] + card[3]
+
+
+def main():
+    card1 = make_trading_card("Borborygmos", "3RRGG", 6, 7)
+    print(card1)
+    card2 = make_trading_card("Shivan Dragon", "4RR", 5, 5)
+    print(card2)
+    card3 = make_trading_card("Magic3", "3RGG", 6, 7)
+    print(card3)
+    card4 = make_trading_card("Magic4", "7G", 3, 7)
+    print(card4)
+    card5 = make_trading_card("Magic5", "0RGG", 6, 9)
+    print(card5)
+
+    print(trading_value(card2))
+
+if __name__  == "__main__":
+    main()
