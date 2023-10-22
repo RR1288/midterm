1f8bc1bf9e67b7ce80e1fc396eb567180567e182
Rubi <rer1288@rit.edu>
2023-10-22 13:52:02 -0400

    question4

--- question3.py
+++ question3.py
@@ -1,11 +1,24 @@
 def make_tuple(a, b, c):
     return tuple([a, b, c])
 
+def reverse_tuple(seq):
+    seq_list = list(seq)
+    rev_list = []
+    while len(seq_list) > 0:
+        rev_list.append(seq_list.pop())
+    return tuple(rev_list)
+
+
+
 def main():
     tup = make_tuple(1,  2, 3)
     print(tup)
-    tup = make_tuple(5, 8, 11)
-    print(tup)
+    rev_tup = reverse_tuple((1,2,3))
+    print(rev_tup)
+    rev_tup = reverse_tuple("Hola")
+    print(rev_tup)
+    rev_tup = reverse_tuple(range(20))
+    print(rev_tup)
 
 if __name__  == "__main__":
     main()
